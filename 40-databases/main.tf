83bb556aef32382821a454ab936cf24ace6b0865
imran <imran806721@gmail.com>
2026-07-07 15:29:37 +0530

    terraform

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -37,3 +37,27 @@ resource "terraform_data" "mongodb" {
   }
 }
 
+resource "terraform_data" "redis" {
+  triggers_replace = [
+    aws_instance.redis.id
+  ]
+
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    password = "DevOps321"
+    host        = aws_instance.redis.private_ip
+  }
+
+  provisioner "file" {
+    source      = "bootstrap.sh"
+    destination = "/tmp/bootstrap.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/bootstrap.sh",
+      "sudo sh /tmp/bootstrap.sh redis ${var.environment}"
+    ]
+  }
+}
