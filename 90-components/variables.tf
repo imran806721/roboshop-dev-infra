81ecfb04761fec5bee60bff422e8aee2cf5cfc87
imran <imran806721@gmail.com>
2026-07-11 15:58:15 +0530

    terraform

--- 90-components/variables.tf
+++ 90-components/variables.tf
@@ -32,5 +32,9 @@ variable "components" {
             rule_priority = 50
             app_version = "v3"
         }
+        frontend = {
+            rule_priority = 10
+            app_version = "v3"
+        }
     }
 }
