83293b8a3a5cd1d9a1e6ee3b50385a917576cf16
imran <imran806721@gmail.com>
2026-07-11 12:16:48 +0530

    terraform

---
+++ 70-acm/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z077495923IYBERYWZVDY"
+}  
+
+variable "domain_name" {
+    default = "devops90s.shop"
+}
