f307179bc2b3a88e661cb8805304820bf33a4739
imran <imran806721@gmail.com>
2026-07-11 14:31:29 +0530

    terraform

---
+++ 90-components/variables.tf
@@ -0,0 +1,19 @@
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z077495923IYBERYWZVDY"
+}  
+
+variable "domain_name" {
+    default = "devops90s.shop"
+}
+
+variable "components" {
+    default = {
+        catalogue = {
+            app_version = "v3"
+        }
+    }
+}
